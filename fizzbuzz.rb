d65e787071ccb03cc324cbae8ea6a8d3fcf9b277
Kalin Jack <kalinj1997@gmail.com>
2021-01-25 13:18:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+
+upto(100)do[i]
+if i % 3 == 0 && i % 5 == 0
+  return "fizzbuzz"
+end
+elseif i % 3 == 0
+  return "fizz"
+end
+elseif i % 5 == 0
+return "buzz"
+end
+
+end
